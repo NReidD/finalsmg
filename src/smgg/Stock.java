a29c687ca2f073e70cf92357f307d8e21671fb64
NReidD <93269958+NReidD@users.noreply.github.com>
2023-03-10 00:53:24 -0500

    runable copy(fix time pleasse)

--- src/smgg/Stock.java
+++ src/smgg/Stock.java
@@ -48,10 +48,14 @@ String key = keys.next();
 System.out.println(key);
 
     JSONObject jArray= jsonObject.getJSONObject(key);
-    JSONObject post_id = jArray.getJSONObject();
-    average = post_id.getInt("SMA");
+    Iterator<String> keysw = jArray.keys();
+    String check = keysw.next();
+    System.out.println(check);
+JSONObject totake = jArray.getJSONObject(check);
+System.out.println(totake.getString("SMA"));
+    average = Double.parseDouble(totake.getString("SMA"));
     System.out.println(average);
-
+return average;
 
 
 } catch (IOException | InterruptedException e) {
@@ -79,8 +83,9 @@ JSONArray pageName = obj.getJSONObject("quoteSummary").getJSONArray("result");
 for (int i = 0; i < pageName.length(); i++) {
     JSONObject post_id = pageName.getJSONObject(i).getJSONObject("price").getJSONObject("marketCap");
     JSONObject post_id2 = pageName.getJSONObject(i).getJSONObject("price").getJSONObject("regularMarketPrice");
-    marketcap = post_id2.getInt("raw");
-    price = post_id.getDouble("raw");
+    marketcap = post_id.getInt("raw");
+    price = post_id2.getDouble("raw");
+    System.out.println("price is "+price);
     return price;
 }
 
@@ -95,7 +100,7 @@ return price;
         return null;
         
     }
-    void buy(int shares, WebDriver driver){
+    void buy(int shares, WebDriver driver) throws InterruptedException{
         System.out.println("Running");
                 WebElement setbuy = driver.findElement(By.xpath("/html/body/div/div[1]/section/section/div/div[1]/div[2]/div[1]/form/div/input[1]"));
                 setbuy.click();
@@ -118,7 +123,8 @@ return price;
                 submit2.click();
                 System.out.println("Executed.");
                 this.shares = shares;
-
+                driver.get("https://www.stockmarketgame.org/enterstock.htm");
+Thread.sleep(50000);
                 }
 
     }
